d038852583f748718836d56eecc20e17c6a83661
GloBoiVic <victorike08@gmail.com>
2023-10-18 10:54:02 -0500

    Create DB model for words

---
+++ backend/src/models/wordModel.ts
@@ -0,0 +1,23 @@
+import { Schema, InferSchemaType, model } from 'mongoose';
+
+const wordSchema = new Schema(
+  {
+    word: {
+      type: String,
+      required: true,
+    },
+    definition: {
+      type: String,
+      required: true,
+    },
+    context: {
+      type: String,
+    },
+    tags: [String],
+  },
+  { timestamps: true },
+);
+
+type Word = InferSchemaType<typeof wordSchema>;
+
+export default model<Word>('Word', wordSchema);
