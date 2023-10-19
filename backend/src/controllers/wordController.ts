5ba19e9912e7af1b20d0c12b737fa93e68215c45
GloBoiVic <victorike08@gmail.com>
2023-10-18 23:21:53 -0500

    Add CRUD functionality

--- backend/src/controllers/wordController.ts
+++ backend/src/controllers/wordController.ts
@@ -1,22 +1,61 @@
 import { RequestHandler } from 'express';
 import axios from 'axios';
+import env from '../utils/validateEnv';
 import wordModel from '../models/wordModel';
 import createHttpError from 'http-errors';
 import mongoose from 'mongoose';
+
+//TODO Add user authentication to routes
+
+export const getWords: RequestHandler = async (req, res, next) => {
+  try {
+    const words = await wordModel.find();
+
+    res.status(200).json({
+      status: 'success',
+      data: words,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getWord: RequestHandler = async (req, res, next) => {
+  const wordId = req.params.wordId;
+  try {
+    if (!mongoose.isValidObjectId(wordId)) throw createHttpError(400, 'Invalid note id');
+
+    const word = await wordModel.findById(wordId);
+
+    if (!word) throw createHttpError(404, 'Word not found');
+
+    res.status(200).json({
+      status: 'success',
+      data: word,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 interface CreateWordBody {
   word: string;
   context?: string;
 }
 
-const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en';
 export const createWord: RequestHandler<unknown, unknown, CreateWordBody, unknown> = async (req, res, next) => {
   const { word, context } = req.body;
 
   try {
     if (!word) throw createHttpError(400, 'Vocabulary word cannot be blank');
 
-    const apiResponse = await axios.get(`${API_URL}/${word}`);
+    const existingWord = await wordModel.findOne({ word: word });
+    console.log(existingWord);
+
+    if (existingWord) throw createHttpError(409, 'This word already exists');
+
+    const apiResponse = await axios.get(`${env.DICTIONARY_API}/${word}`);
+
     const definition = apiResponse.data[0].meanings[0].definitions[0].definition;
 
     //TODO: Grab parts of speech and add it to tags
@@ -26,7 +65,75 @@ export const createWord: RequestHandler<unknown, unknown, CreateWordBody, unknow
       definition,
       context,
     });
-    res.status(201).json(newWord);
+    res.status(201).json({
+      status: 'success',
+      data: newWord,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface UpdateWordParams {
+  wordId: string;
+}
+
+interface UpdateWordBody {
+  word: string;
+  context?: string;
+}
+
+export const updateWord: RequestHandler<UpdateWordParams, unknown, UpdateWordBody, unknown> = async (
+  req,
+  res,
+  next,
+) => {
+  const wordId = req.params.wordId;
+  const newWord = req.body.word;
+  const newContext = req.body.context;
+
+  try {
+    if (!mongoose.isValidObjectId(wordId)) throw createHttpError(400, 'Invalid note id');
+    if (!newWord) throw createHttpError(400, 'Vocabulary word cannot be blank');
+
+    const word = await wordModel.findById(wordId);
+
+    if (!word) throw createHttpError(404, ' Word not found');
+
+    const apiResponse = await axios.get(`${env.DICTIONARY_API}/${newWord}`);
+
+    const definition = apiResponse.data[0].meanings[0].definitions[0].definition;
+
+    word.word = newWord;
+    word.definition = definition;
+    word.context = newContext;
+
+    const updatedWord = await word.save();
+
+    res.status(200).json({
+      status: 'success',
+      data: updatedWord,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteWord: RequestHandler = async (req, res, next) => {
+  const wordId = req.params.wordId;
+
+  try {
+    if (!mongoose.isValidObjectId(wordId)) throw createHttpError(400, 'Invalid note id');
+
+    const word = await wordModel.findById(wordId);
+
+    if (!word) throw createHttpError(404, 'Word not found');
+
+    await word.deleteOne();
+
+    res.status(204).json({
+      status: 'success',
+    });
   } catch (error) {
     next(error);
   }
--- backend/src/routes/wordRouter.ts
+++ backend/src/routes/wordRouter.ts
@@ -3,6 +3,10 @@ import * as wordController from '../controllers/wordController';
 
 const router = express.Router();
 
+router.get('/', wordController.getWords);
 router.post('/', wordController.createWord);
+router.get('/:wordId', wordController.getWord);
+router.patch('/:wordId', wordController.updateWord);
+router.delete('/:wordId', wordController.deleteWord);
 
 export default router;
