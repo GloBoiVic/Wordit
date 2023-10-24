b5d0ab18f9bb0556684c697836af491462f054bc
GloBoiVic <victorike08@gmail.com>
2023-10-24 10:06:46 -0500

    Update components

--- frontend/src/features/vocab/Vocab.tsx
+++ frontend/src/features/vocab/Vocab.tsx
@@ -3,19 +3,27 @@ import { Vocab as VocabModel } from '../../models/vocab';
 import * as WordsApi from '../../services/api';
 import Sidebar from '../../ui/Sidebar';
 import VocalCard from './VocalCard';
+import CreateWordModal from './CreateWordModal';
 
 function Vocab() {
+  const [wordsLoading, setWordsLoading] = useState(true);
   const [showAddWordModal, setShowAddWordModal] = useState(false);
   const [words, setWords] = useState<VocabModel[]>([]);
+  const [showWordLoadingError, setShowWordLoadingError] = useState(false);
 
   useEffect(() => {
     const fetchWords = async () => {
       try {
+        setShowWordLoadingError(false);
+        setWordsLoading(true);
         const data = await WordsApi.fetchWords();
 
         setWords(data);
       } catch (error) {
         console.error(error);
+        setShowWordLoadingError(true);
+      } finally {
+        setWordsLoading(false);
       }
     };
     fetchWords();
@@ -24,7 +32,17 @@ function Vocab() {
   return (
     <div className="container">
       <div className="flex flex-col grid-cols-4 gap-6 sm:grid">
-        <Sidebar />
+        <Sidebar onDismiss={() => setShowAddWordModal(true)}>
+          {showAddWordModal && (
+            <CreateWordModal
+              onDismiss={() => setShowAddWordModal(false)}
+              onWordSaved={(newWord) => {
+                setWords([...words, newWord]);
+                setShowAddWordModal(false);
+              }}
+            />
+          )}
+        </Sidebar>
         <main className="grid col-span-3 gap-2 lg:grid-cols-3 sm:grid-cols-2">
           {words.length > 0 && words.map((word) => <VocalCard word={word} key={word._id} />)}
         </main>
--- frontend/src/ui/Sidebar.tsx
+++ frontend/src/ui/Sidebar.tsx
@@ -1,22 +1,24 @@
 import { Button, Input } from '@nextui-org/react';
-import CreateWordModal from '../features/vocab/CreateWordModal';
-import { useState } from 'react';
+import { ReactNode } from 'react';
 
-function Sidebar() {
-  const [showAddWordModal, setShowAddWordModal] = useState(false);
+type SidebarProps = {
+  onDismiss: () => void;
+  children: ReactNode;
+};
 
+function Sidebar({ onDismiss, children }: SidebarProps) {
   return (
     <>
       <div className="w-full col-span-1 px-2 py-4 bg-gray-300">
         <div className="w-3/4 space-y-3">
-          <Button onPress={() => setShowAddWordModal(true)} className="w-full border">
+          <Button onPress={onDismiss} className="w-full border">
             Add word
           </Button>
           <Input placeholder="Search..." type="text" radius="full" />
         </div>
       </div>
 
-      {showAddWordModal && <CreateWordModal onDismiss={() => setShowAddWordModal(false)} />}
+      {children}
     </>
   );
 }
