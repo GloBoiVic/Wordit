c7ef2e5678f754396e551eab31b26b8851d20426
GloBoiVic <victorike08@gmail.com>
2023-10-27 10:18:13 -0500

    Update word routes

--- backend/src/routes/wordRoutes.ts
+++ backend/src/routes/wordRoutes.ts
@@ -4,9 +4,13 @@ import * as wordController from '../controllers/wordController';
 const router = express.Router();
 
 router.get('/', wordController.getWords);
-router.post('/', wordController.createWord);
+
 router.get('/:wordId', wordController.getWord);
+
+router.post('/', wordController.createWord);
+
 router.patch('/:wordId', wordController.updateWord);
+
 router.delete('/:wordId', wordController.deleteWord);
 
 export default router;
