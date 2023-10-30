66c6b76bfe360fe64b38d240dd2d4eccbf4aba89
GloBoiVic <victorike08@gmail.com>
2023-10-30 11:05:44 -0500

    Update links

--- frontend/src/ui/Header.tsx
+++ frontend/src/ui/Header.tsx
@@ -4,14 +4,18 @@ import { NavLink } from 'react-router-dom';
 function Header() {
   return (
     <header className="flex items-center justify-between w-full h-12 px-4 py-2 mx-auto border">
-      <h1 className="text-4xl font-bold leading-relaxed uppercase">Wordit</h1>
+      <Link as={NavLink} to="/">
+        <h1 className="text-4xl font-bold leading-relaxed uppercase">Wordit</h1>
+      </Link>
 
       <ul className="flex gap-8">
         {/* TODO: Add proper routes to links */}
         <Link as={NavLink} to="/users/signup">
           Sign Up
         </Link>
-        <Link as={NavLink}>Log In</Link>
+        <Link as={NavLink} to="/users/login">
+          Log In
+        </Link>
       </ul>
     </header>
   );
