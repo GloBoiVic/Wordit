b7c0d4c1b1c39ff7c2aed9fa73ea65590c60d19b
GloBoiVic <victorike08@gmail.com>
2023-10-30 09:12:27 -0500

    Add cookie parser and sessions

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,14 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import wordRouter from './routes/wordRoutes';
 import userRouter from './routes/userRoutes';
+import session from 'express-session';
+import env from './utils/validateEnv';
+import MongoStore from 'connect-mongo';
+import createHttpError, { isHttpError } from 'http-errors';
+import cookieParser from 'cookie-parser';
+import { requiresAuth } from './middlewares/auth';
 
 const app = express();
 
@@ -12,8 +18,42 @@ app.use(cors());
 
 app.use(morgan('dev'));
 
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000, // 60 minutes
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGO_CONNECTION_STRING,
+    }),
+  }),
+); // Register session
+
 app.use('/api/v1/users', userRouter);
 
-app.use('/api/v1/words', wordRouter);
+app.use('/api/v1/words', requiresAuth, wordRouter);
+
+app.use((req, res, next) => {
+  next(createHttpError(404, 'Endpoint not found'));
+});
+
+// Global Error Handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = 'An unknown error occured';
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
+});
 
 export default app;
