c191f1278a9b59f3e114bfbdfd9fef1d079188cb
GloBoiVic <victorike08@gmail.com>
2023-10-30 09:12:00 -0500

    Add helper fn for session ids

---
+++ backend/src/utils/assertIsDefined.ts
@@ -0,0 +1,7 @@
+// Function to check is the value is not null or undefined
+
+export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
+  if (!val) {
+    throw Error(`Expected 'val' to be defined, but received ${val}`);
+  }
+}
