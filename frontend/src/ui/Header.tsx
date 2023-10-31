627eafc22530a5ebbcae828c341690e282538238
GloBoiVic <victorike08@gmail.com>
2023-10-31 10:06:25 -0500

    Add loggedin and loggedout views

--- frontend/src/ui/Header.tsx
+++ frontend/src/ui/Header.tsx
@@ -1,7 +1,26 @@
 import { Link } from '@nextui-org/react';
 import { NavLink } from 'react-router-dom';
+import useLoggedInUser from '../hooks/useLoggedInUser';
+import NavBarLoggedInPage from '../features/user/NavBarLoggedInPage.tsx';
+import { useEffect } from 'react';
+import * as WordsApi from '../services/api';
+import NavBarLoggedOutPage from '../features/user/NavBarLoggedOutPage.tsx';
 
 function Header() {
+  const { loggedInUser, setLoggedInUser } = useLoggedInUser();
+  console.log(loggedInUser);
+
+  useEffect(() => {
+    async function fetchLoggedInUser() {
+      try {
+        const user = await WordsApi.getLoggedInUser();
+        setLoggedInUser(user);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchLoggedInUser();
+  }, [setLoggedInUser]);
   return (
     <header className="flex items-center justify-between w-full h-12 px-4 py-2 mx-auto border">
       <Link as={NavLink} to="/">
@@ -9,13 +28,15 @@ function Header() {
       </Link>
 
       <ul className="flex gap-8">
-        {/* TODO: Add proper routes to links */}
-        <Link as={NavLink} to="/users/signup">
-          Sign Up
-        </Link>
-        <Link as={NavLink} to="/users/login">
-          Log In
-        </Link>
+        {loggedInUser ? (
+          <NavBarLoggedInPage
+            user={loggedInUser}
+            onLogoutSuccessful={() => setLoggedInUser(null)}
+          />
+        ) : (
+          <NavBarLoggedOutPage />
+        )}
+        {/* {!loggedInUser && <NavBarLoggedOutPage />} */}
       </ul>
     </header>
   );
