f0693b883785d891a88b722f61adacf665febb32
GloBoiVic <victorike08@gmail.com>
2023-11-05 17:31:34 -0600

    Update custom hook

--- frontend/src/features/vocab/useGetWords.ts
+++ frontend/src/features/vocab/useGetWords.ts
@@ -1,7 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchWords } from '../../services/api';
+import { ChangeEvent, useCallback, useState } from 'react';
+import { VocabModel } from '../../models/vocabModel';
 
 function useGetWords() {
+  // const [searchQuery, setSearchQuery] = useState('');
+
+  // const handleSearchQuery = (e: ChangeEvent<HTMLInputElement>) => {
+  //   e.preventDefault();
+  //   setSearchQuery(e.target.value);
+  // };
+
+  // const filteredWords = useCallback(
+  //   (words: VocabModel[]) => {
+  //     if (!searchQuery) return words;
+
+  //     return words.filter((word) => word.word.includes(searchQuery.toLowerCase()));
+  //   },
+  //   [searchQuery],
+  // );
   const {
     isLoading,
     data: words,
@@ -9,6 +26,7 @@ function useGetWords() {
   } = useQuery({
     queryKey: ['words'],
     queryFn: fetchWords,
+    // select: filteredWords,
   });
 
   return { isLoading, words, error };
