ec17e0412ae5c30abddff398f5cefd6286eeb7ff
GloBoiVic <victorike08@gmail.com>
2023-11-06 13:02:12 -0600

    Updater custom hook

--- frontend/src/features/vocab/useGetWords.ts
+++ frontend/src/features/vocab/useGetWords.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchWords } from '../../services/api';
 import { ChangeEvent, useCallback, useState } from 'react';
 import { VocabModel } from '../../models/vocabModel';
+import { fetchWords } from '../../services/api';
 
 function useGetWords() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -15,27 +15,27 @@ function useGetWords() {
     (words: VocabModel[]) => {
       if (!searchQuery) return words;
 
-      return (
-        searchQuery.length &&
-        words.filter((word: VocabModel) =>
-          word.word.toLowerCase().includes(searchQuery.toLowerCase()),
-        )
+      return words.filter((word: VocabModel) =>
+        word.word.toLowerCase().includes(searchQuery.toLowerCase()),
       );
     },
     [searchQuery],
   );
+
   const {
     isLoading,
     data: words = [],
     error,
   } = useQuery({
-    queryKey: ['words'],
+    queryKey: ['words', searchQuery],
     queryFn: fetchWords,
-    // select: (words) =>
-    //   words.filter((word: VocabModel) =>
+    // select: (words) => {
+    //   console.log(words);
+    //   return words.filter((word: VocabModel) =>
     //     word.word.toLowerCase().includes(searchQuery.toLowerCase()),
-    //   ),
-    // select: filteredWords,
+    //   );
+    // },
+    select: filteredWords,
     refetchOnWindowFocus: false,
     retry: 2,
   });
