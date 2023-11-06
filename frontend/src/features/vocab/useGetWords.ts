b90c02321396d59a0382f74414aba267248eea61
GloBoiVic <victorike08@gmail.com>
2023-11-06 10:50:00 -0600

    Update custom hook

--- frontend/src/features/vocab/useGetWords.ts
+++ frontend/src/features/vocab/useGetWords.ts
@@ -4,32 +4,43 @@ import { ChangeEvent, useCallback, useState } from 'react';
 import { VocabModel } from '../../models/vocabModel';
 
 function useGetWords() {
-  // const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
 
-  // const handleSearchQuery = (e: ChangeEvent<HTMLInputElement>) => {
-  //   e.preventDefault();
-  //   setSearchQuery(e.target.value);
-  // };
+  const handleSearchQuery = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearchQuery(e.target.value);
+  };
 
-  // const filteredWords = useCallback(
-  //   (words: VocabModel[]) => {
-  //     if (!searchQuery) return words;
+  const filteredWords = useCallback(
+    (words: VocabModel[]) => {
+      if (!searchQuery) return words;
 
-  //     return words.filter((word) => word.word.includes(searchQuery.toLowerCase()));
-  //   },
-  //   [searchQuery],
-  // );
+      return (
+        searchQuery.length &&
+        words.filter((word: VocabModel) =>
+          word.word.toLowerCase().includes(searchQuery.toLowerCase()),
+        )
+      );
+    },
+    [searchQuery],
+  );
   const {
     isLoading,
-    data: words,
+    data: words = [],
     error,
   } = useQuery({
     queryKey: ['words'],
     queryFn: fetchWords,
+    // select: (words) =>
+    //   words.filter((word: VocabModel) =>
+    //     word.word.toLowerCase().includes(searchQuery.toLowerCase()),
+    //   ),
     // select: filteredWords,
+    refetchOnWindowFocus: false,
+    retry: 2,
   });
 
-  return { isLoading, words, error };
+  return { isLoading, words, error, searchQuery, handleSearchQuery };
 }
 
 export default useGetWords;
